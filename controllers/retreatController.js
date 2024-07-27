aff5da353889b34f5a5d84e969c5e5b1198f089c
Mohammed Hamza <211220035@nitdelhi.ac.in>
2024-07-28 01:23:52 +0530

    Search Done

--- controllers/retreatController.js
+++ controllers/retreatController.js
@@ -9,3 +9,24 @@ exports.getAllRetreats = async (req, res) => {
     res.status(500).json(err.message);
   }
 };
+
+// Get retreat by search
+exports.getRetreatBySearch = async (req, res) => {
+  try {
+    const { search } = req.query;
+      const searchTerm = `%${search}%`;
+    const retreats = await pool.query(
+      `SELECT * FROM retreats 
+           WHERE title ILIKE $1 
+           OR description ILIKE $1 
+           OR location ILIKE $1
+           OR CONDITION ILIKE $1
+           OR tags::text ILIKE $1`,
+      [searchTerm]
+    );
+    res.status(200).json(retreats.rows);
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- routes/retreatRoutes.js
+++ routes/retreatRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const { getRetreatBySearch } = require("../controllers/retreatController");
 const { getAllRetreats } = require("../controllers/retreatController");
 
 router.get("/", getAllRetreats);
-
+// controller for get retreat by search
+router.get("/search", getRetreatBySearch);
 module.exports = router;
